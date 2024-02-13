4818c48a539a488ab7d718bafc18acfec1c905df
Thalles Oliveira <thalles.gbpereira@gmail.com>
2024-02-13 13:45:06 -0300

    Criação entity usuario e editorconfig chatao

---
+++ src/entities/usuarios.js
@@ -0,0 +1,14 @@
+export const usuario = {
+        get: {
+                email: "",
+                nome: "",
+                take: 20,
+                skip: 0
+        },
+
+        insertOrUpdate: {
+                nome: "",
+                email: "",
+                senha: ""
+        }
+}
